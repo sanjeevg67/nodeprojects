496bb440241ad26d273c80159f3c86675bb4dfae
Sanjeev Goel <sanjeevg67@yahoo.com>
2017-09-18 14:01:46 +0530

    Uploaded from Mac

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Cloud Function.
+ *
+ * @param {object} event The Cloud Functions event.
+ * @param {function} callback The callback function.
+ */
+exports.helloWorld = function helloWorld (event, callback) {
+  console.log(`My Cloud Function: ${event.data.message}`);
+  callback();
+};
